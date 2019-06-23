63cf4da71911f9c238c96610b44482d04206bfdd
Szalwinski <dawidx.szalwinski@intel.com>
2019-06-23 21:38:56 +0200

    Changed script structure.

--- source_script.py
+++ source_script.py
@@ -1,12 +1,8 @@
 import json
 import matplotlib.pyplot as plt
 
-if __name__ == '__main__':
-    DataFileName = 'dataMay-31-2017.json'
 
-    with open(DataFileName, 'r') as DataFile:
-        collectedData = json.load(DataFile)['data']
-
+def count_occurances(collectedData):
     names_occ, company_occ, city_occ = {}, {}, {}
     for record in collectedData:
         name = record[3]
@@ -22,22 +18,28 @@ if __name__ == '__main__':
             city_occ[city] += 1
 
         company = record[1]
-        if name not in company_occ:
+        if company not in company_occ:
             company_occ[company] = 1
         else:
             company_occ[company] += 1
 
-    plt.title('Names')
-    plt.bar(*zip(*names_occ.items()))
+    return names_occ, company_occ, city_occ
+
+
+def show_plot(data, title):
+    plt.title(title)
+    plt.bar(*zip(*data.items()))
     plt.xticks(rotation=90, fontsize=5)
     plt.show()
 
-    plt.title('Cities')
-    plt.bar(*zip(*city_occ.items()))
-    plt.xticks(rotation=90, fontsize=5)
-    plt.show()
 
-    plt.title('Companies')
-    plt.bar(*zip(*company_occ.items()))
-    plt.xticks(rotation=90, fontsize=5)
-    plt.show()
+if __name__ == '__main__':
+    DataFileName = 'dataMay-31-2017.json'
+
+    with open(DataFileName, 'r') as DataFile:
+        collectedData = json.load(DataFile)['data']
+
+    names_occ, company_occ, city_occ = count_occurances(collectedData)
+    show_plot(names_occ, 'Names')
+    show_plot(company_occ, 'Companies')
+    show_plot(city_occ, 'Cities')
